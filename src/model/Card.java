8e46bbffa09936dd314c7957d9559fbdac279358
Sander Cokart <cokart32@gmail.com>
2019-04-01 11:57:42 +0200

    Start of final

---
+++ src/model/Card.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Card {
+
+}
+
+class Card_1 extends Card{
+
+}
+
+class Card_2 extends Card{
+
+}
+
+class Card_3 extends Card{
+
+}
+
+class Card_4 extends Card{
+
+}
+
+class Card_5 extends Card{
+
+}
+
+class Card_6 extends Card{
+
+}
+
+class Card_7 extends Card{
+
+}
+
+class Card_8 extends Card{
+
+}
