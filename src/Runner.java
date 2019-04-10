1029d8775b3cc0ec266a7cfb1eca55f86a7cb152
Sander Cokart <cokart32@gmail.com>
2019-04-10 22:31:19 +0200

    Final 1 is done!

---
+++ src/Runner.java
@@ -0,0 +1,17 @@
+import controller.GameController;
+import model.GameField;
+import view.GameView;
+import view.WinView;
+
+public class Runner {
+    public static void main(String[] args) {//MAIN;
+        run();//runs method run;
+    }
+
+    public static void run(){//method to run the game
+        GameField game_field_object = new GameField();//creates a new game field;
+        GameView game_view_object = new GameView();//creates a user interface;
+        WinView win_view = new WinView();//creates a new winner interface;
+        GameController game_controller_object = new GameController(game_view_object, game_field_object, win_view);//creates the controller to interact with the different objects as parameters
+    }
+}
--- src/model/Card.java
+++ src/model/Card.java
@@ -1,37 +1,57 @@
 package model;
 
+import javax.swing.*;
+
 public class Card {
+    private int cardNumber;//the cardNumber assigned to this card;
+    private JButton jButton;//the jButton that is assigned to this card;
 
-}
+    public Card(int cardNumber, JButton jButton) {//upon creation;
+        this.cardNumber = cardNumber;//set the card number of this card;
+        this.jButton = jButton;//set the JButton of this card;
+//        System.out.println(cardNumber);//SOUT the card numbers;
+    }
 
-class Card_1 extends Card{
 
-}
 
-class Card_2 extends Card{
 
-}
+    public void flipCard() {//method to flip a card;
+        if (jButton.getIcon() == null){//if the icon compares to null;
+            switch (cardNumber) {//set the image of corresponding to the cardNumber
+                case 1:
+                    jButton.setIcon(new ImageIcon("C:\\Users\\cokar\\OneDrive - ROC Drenthe College\\Drenthe College Van Schaikweg\\IT\\Java A3 Final\\src\\resources\\card_1.jpg"));
+                    break;
+                case 2:
+                    jButton.setIcon(new ImageIcon("C:\\Users\\cokar\\OneDrive - ROC Drenthe College\\Drenthe College Van Schaikweg\\IT\\Java A3 Final\\src\\resources\\card_2.jpg"));
+                    break;
+                case 3:
+                    jButton.setIcon(new ImageIcon("C:\\Users\\cokar\\OneDrive - ROC Drenthe College\\Drenthe College Van Schaikweg\\IT\\Java A3 Final\\src\\resources\\card_3.jpg"));
+                    break;
+                case 4:
+                    jButton.setIcon(new ImageIcon("C:\\Users\\cokar\\OneDrive - ROC Drenthe College\\Drenthe College Van Schaikweg\\IT\\Java A3 Final\\src\\resources\\card_4.jpg"));
+                    break;
+                case 5:
+                    jButton.setIcon(new ImageIcon("C:\\Users\\cokar\\OneDrive - ROC Drenthe College\\Drenthe College Van Schaikweg\\IT\\Java A3 Final\\src\\resources\\card_5.jpg"));
+                    break;
+                case 6:
+                    jButton.setIcon(new ImageIcon("C:\\Users\\cokar\\OneDrive - ROC Drenthe College\\Drenthe College Van Schaikweg\\IT\\Java A3 Final\\src\\resources\\card_6.jpg"));
+                    break;
+                case 7:
+                    jButton.setIcon(new ImageIcon("C:\\Users\\cokar\\OneDrive - ROC Drenthe College\\Drenthe College Van Schaikweg\\IT\\Java A3 Final\\src\\resources\\card_7.jpg"));
+                    break;
+                case 8:
+                    jButton.setIcon(new ImageIcon("C:\\Users\\cokar\\OneDrive - ROC Drenthe College\\Drenthe College Van Schaikweg\\IT\\Java A3 Final\\src\\resources\\card_8.jpg"));
+                    break;
+            }
+        } else {//if the JButton icon does not compare to null;
+            jButton.setIcon(null);//set the text to null;
+        }
 
-class Card_3 extends Card{
 
-}
 
-class Card_4 extends Card{
+    }
 
-}
-
-class Card_5 extends Card{
-
-}
-
-class Card_6 extends Card{
-
-}
-
-class Card_7 extends Card{
-
-}
-
-class Card_8 extends Card{
-
+    public int getCardNumber() {//returns the cardNumber of this card;
+        return cardNumber;
+    }
 }
